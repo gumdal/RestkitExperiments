913142b830344eef74bc6c9cda448e3a55b2452c
Raj Pawan <rpgumdal@crri.co.in>
2012-06-26 15:19:10 +0530

    Initial Commit

---
+++ RestKitExperiment/RKEViewController.h
@@ -0,0 +1,13 @@
+//
+//  RKEViewController.h
+//  RestKitExperiment
+//
+//  Created by Raj Pawan Gumdal on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKEViewController : UIViewController
+
+@end
---
+++ RestKitExperiment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestKitExperiment
+//
+//  Created by Raj Pawan Gumdal on 6/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RKEAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RKEAppDelegate class]));
+    }
+}
